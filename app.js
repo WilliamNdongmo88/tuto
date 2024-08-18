12a1b2fee983b0c95d38d6651cd7469f26d4dc4d
unknown <WilliamNdongmo88>
2024-08-18 23:38:00 +0200

    test1

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ app.use((req, res, next) => {
 
   
   app.use((req, res) => {
-    res.json({ message: "UPDATE !" }); 
+    res.json([
+        { "id": 1, "nom": "Bafoussam", "population": 285000, "departement": "Menoua" },
+        { "id": 2, "nom": "Dschang", "population": 120000, "departement": "Menoua" },
+        { "id": 3, "nom": "Mbouda", "population": 90000, "departement": "Bamboutos" },
+        { "id": 4, "nom": "Bangangté", "population": 65000, "departement": "Ndé" },
+        { "id": 5, "nom": "Foumban", "population": 100000, "departement": "Noun" },
+        { "id": 6, "nom": "Foumbot", "population": 55000, "departement": "Noun" },
+        { "id": 7, "nom": "Kékem", "population": 35000, "departement": "Haut-Nkam" },
+        { "id": 8, "nom": "Makénéné", "population": 28000, "departement": "Haut-Nkam" },
+        { "id": 9, "nom": "Bangou", "population": 20000, "departement": "Méfi" },
+        { "id": 10, "nom": "Bafang", "population": 60000, "departement": "Haut-Nkam" }
+    ]); 
  });
 
 module.exports = app;
